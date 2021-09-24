aca49f85e08cb21127128ff8510f88c013bbad81
Jayson Cabrillas San Buenaventura <sanbuenaventurajayson28@gmail.com>
2021-09-24 19:04:38 +0800

    Update sched.py

--- sched.py
+++ sched.py
@@ -1,94 +1,205 @@
 #!/usr/bin/python3
-#-*- coding: utf-8 -*-
 
-# LIBRARIES
 import os
-import time
+import sys
+from time import sleep
 
-# FOREGROUND & BACKGROUND COLORS
-#    black='\033[30m'
-#    red='\033[31m'
-#    green='\033[32m'
-#    orange='\033[33m'
-#    blue='\033[34m'
-#    purple='\033[35m'
-#    cyan='\033[36m'
-#    lightgrey='\033[37m'
-#    darkgrey='\033[90m'
-#    lightred='\033[91m'
-#    lightgreen='\033[92m'
-#    yellow='\033[93m'
-#    lightblue='\033[94m'
-#    pink='\033[95m'
-#    lightcyan='\033[96m'
+class bcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-def my_sched():
+def slowprint(s):
+	for c in s + '\n':
+		sys.stdout.write(c)
+		sys.stdout.flush()
+		sleep(.01/10)
+
+def main():
     os.system("clear")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    os.system("toilet -f smbraille 'BSIT-WMAD SCHEDULE'")
     print("")
-    print("\033[37m ┌──────────────┬──────────────────────────────────────┬──────┬─────────┬────────────┬──────┬─────────────────────┬──────┬────────────────┐  ┌────────────────────────┐")
-    time.sleep(0.2)
-    print("\033[37m │\033[33m Subject Code\033[37m │          \033[33mDescriptive title\033[37m           │ \033[33mUnit\033[37m │ \033[33mSection\033[37m │   \033[33mCourse\033[37m   │ \033[33mDay\033[37m  │        \033[33mTime\033[37m         │ \033[33mRoom\033[37m │   \033[33mInstructor\033[37m   │  │ MONDAY     -    \033[94mBLUE\033[37m   │")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤  ├────────────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │\033[91m    GEC 107   \033[37m│   \033[91mScience, Technology and Society    \033[37m│  \033[91m3   \033[37m│  \033[91mII-A   \033[37m│ \033[91mBSINFOTECH \033[37m│ \033[91mTUES \033[37m│ \033[91m01:00 PM - 04:00 PM \033[37m│ \033[91mTBA  \033[37m│ \033[91mA. Banasihan   \033[37m│  │ TUESDAY    -    \033[91mRED\033[37m    │")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤  ├────────────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │\033[94m   ITEC 204   \033[37m│   \033[94mData Structures and Algorithm      \033[37m│  \033[94m3   \033[37m│  \033[94mII-A   \033[37m│ \033[94mBSINFOTECH \033[37m│ \033[94mMOND \033[37m│ \033[94m07:30 AM - 09:30 AM \033[37m│ \033[94mTBA  \033[37m│ \033[94mM. Cardona     \033[37m│  │ WEDNESDAY  -    \033[33mORANGE\033[37m │")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤  ├────────────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │              │                                      │      │\033[33m  II-A   \033[37m│\033[33m BSINFOTECH \033[37m│\033[33m WEDN \033[37m│\033[33m 01:00 PM - 04:00 PM \033[37m│\033[33m TBA  \033[37m│\033[33m M. Cardona     \033[37m│  │ THURSDAY   -    WHITE  │")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤  ├────────────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │\033[94m   ITEC 205   \033[37m│\033[94m   Information Management             \033[37m│\033[94m  3   \033[37m│\033[94m  II-A   \033[37m│\033[94m BSINFOTECH \033[37m│\033[94m MOND \033[37m│\033[94m 09:30 AM - 11:30 AM \033[37m│\033[94m TBA  \033[37m│\033[94m R. Elomina     \033[37m│  │ FRIDAY     -    \033[93mYELLOW\033[37m │")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤  └────────────────────────┘")
-    time.sleep(0.2)
-    print("\033[37m │              │                                      │      │\033[93m  II-A   \033[37m│\033[93m BSINFOTECH \033[37m│\033[93m FRID \033[37m│\033[93m 08:00 AM - 11:00 AM \033[37m│\033[93m TBA  \033[37m│\033[93m R. Elomina     \033[37m│")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │\033[94m   ITEL 201   \033[37m│\033[94m   Object-Oriented Programming        \033[37m│\033[94m  3   \033[37m│\033[94m  II-A   \033[37m│\033[94m BSINFOTECH \033[37m│\033[94m MOND \033[37m│\033[94m 03:00 PM - 05:00 PM \033[37m│\033[94m TBA  \033[37m│\033[94m G. Catedrilla  \033[37m│")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │              │                                      │      │  II-A   │ BSINFOTECH │ THUR │ 01:00 PM - 04:00 PM │ TBA  │ G. Catedrilla  │")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │\033[33m   ITEL 202   \033[37m│\033[33m   Platform Technologies              \033[37m│\033[33m  3   \033[37m│\033[33m  II-A   \033[37m│\033[33m BSINFOTECH \033[37m│\033[33m WEDN \033[37m│\033[33m 07:30 AM - 10:30 AM \033[37m│\033[33m TBA  \033[37m│\033[33m W. Suyat       \033[37m│")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │              │                                      │      │  II-A   │ BSINFOTECH │ THUR │ 07:30 AM - 09:30 AM │ TBA  │ W. Suyat       │")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │\033[94m   ITEP 203   \033[37m│\033[94m   Quantitative Metdhods              \033[37m│\033[94m  3   \033[37m│\033[94m  II-A   \033[37m│\033[94m BSINFOTECH \033[37m│\033[94m MOND \033[37m│\033[94m 01:00 PM - 03:00 PM \033[37m│\033[94m TBA  \033[37m│\033[94m J. Asor        \033[37m│")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │              │                                      │      │\033[33m  II-A   \033[37m│\033[33m BSINFOTECH \033[37m│\033[33m WEDN \033[37m│\033[33m 04:00 PM - 05:00 PM \033[37m│\033[33m TBA  \033[37m│\033[33m J. Asor        \033[37m│")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │\033[93m   PHED 213   \033[37m│\033[93m   Individual/Dual Sports Games       \033[37m│\033[93m  2   \033[37m│\033[93m  II-A   \033[37m│\033[93m BSINFOTECH \033[37m│\033[93m FRID \033[37m│\033[93m 03:00 PM - 05:00 PM \033[37m│\033[93m TBA  \033[37m│\033[93m P. Paruan      \033[37m│")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │\033[91m  SOSLIT 103  \033[37m│\033[91m   Sosyedad at Literatura/Panitikan   \033[37m│\033[91m  3   \033[37m│\033[91m  II-A   \033[37m│\033[91m BSINFOTECH \033[37m│\033[91m TUES \033[37m│\033[91m 04:00 PM - 05:00 PM \033[37m│\033[91m TBA  \033[37m│\033[91m J. Lerios      \033[37m│")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┼──────────────────────────────────────┼──────┼─────────┼────────────┼──────┼─────────────────────┼──────┼────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │              │                                      │      │  II-A   │ BSINFOTECH │ THUR │ 09:30 AM - 11:30 AM │ TBA  │ J. Lerios      │")
-    time.sleep(0.2)
-    print("\033[37m ├──────────────┴──────────────────────────────────────┴──────┴─────────┴────────────┴──────┴─────────────────────┴──────┴────────────────┤")
-    time.sleep(0.2)
-    print("\033[37m │                                          TOTAL UNITS : 23                                                                              │")
-    time.sleep(0.2)
-    print("\033[37m └────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " [01] Monday Schedule")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " [02] Tuesday Schedule")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " [03] Wednesday Schedule")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " [04] Thursday Schedule")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " [05] Friday Schedule")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " [06] Personal Infos")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " [00] Close Schedule")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
 
-my_sched()
+    day = input(" ~> ")
+
+    if day == '1' or day == '01':
+        monday()
+    elif day == '2' or day == '02':
+        tuesday()
+    elif day == '3' or day == '03':
+        wednesday()
+    elif day == '4' or day == '04':
+        thursday()
+    elif day == '5' or day == '05':
+        friday()
+    elif day == '6' or day == '06':
+        about()
+    elif day == '0' or day == '00':
+        os.system("clear && exit")
+    else:
+        os.system("clear")
+        print(" Invalid input!")
+
+    
+def about():
+    os.system("clear")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    os.system("toilet -f smbraille 'PERSONAL INFORMATION'")
+    slowprint(bcolors.BOLD + bcolors.GREEN + "\n")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " STUDENT NO. :    0419-0678")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " NAME        :    Jayson Cabrillas San Buenaventura")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " COURSE      :    BSIT-WMAD")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " SECTION     :    BSIT-3A")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " YEAR        :    3rd Year")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " TYPE        :    Regular")
+    slowprint(bcolors.BOLD + bcolors.GREEN + " GENDER      :    Male")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    opt = input(" Back to main (y/n): ")
+
+    if opt == 'y' or opt == 'Y':
+        main()
+    elif opt == 'n' or opt == 'N':
+        os.system("clear && exit")
+    else:
+        os.system("clear")
+        print(" Invalid input!")
+
+
+def monday():
+    os.system("clear")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    os.system("toilet -f smbraille '            MONDAY SCHEDULE'")
+    print("")
+    slowprint(bcolors.BOLD + bcolors.GREEN + "\n ┌──────────────┬───────────────────────────┬──────┬───────────────────────┬────────────────┐")
+    slowprint(" │ Subject Code │  Descriptive Title        │ Day  │         Time          │  Instructor    │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITEP 308     │  System Integration       │ MON  │  07:30 AM - 10:30 AM  │  A. Cortez     │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITEL 304     │  Integrative Programming  │ MON  │  12:00 PM - 03:00 PM  │  H. Natividad  │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITEP 310     │  Social and Pro. Issues   │ MON  │  03:00 PM - 05:00 PM  │  M. Camba      │")
+    slowprint(" └──────────────┴───────────────────────────┴──────┴───────────────────────┴────────────────┘")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    opt = input(" Back to main (y/n): ")
+
+    if opt == 'y' or opt == 'Y':
+        main()
+    elif opt == 'n' or opt == 'N':
+        os.system("clear && exit")
+    else:
+        os.system("clear")
+        print(" Invalid input!")
+
+
+def tuesday():
+    os.system("clear")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    os.system("toilet -f smbraille '           TUESDAY SCHEDULE'") 
+    print("")
+    slowprint(bcolors.BOLD + bcolors.GREEN + "\n ┌──────────────┬───────────────────────────┬──────┬───────────────────────┬────────────────┐")
+    slowprint(" │ Subject Code │  Descriptive Title        │ Day  │         Time          │  Instructor    │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITEP 309     │  Networking 2             │ TUE  │  07:00 AM - 10:00 AM  │  H. Natividad  │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITST 302     │  Client-Server Techs.     │ TUE  │  10:30 AM - 12:30 AM  │  A. Cortez     │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITEP 308     │  System Integration       │ TUE  │  01:00 PM - 03:00 PM  │  A. Cortez     │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITST 301     │  Principle of Web Design  │ TUE  │  03:00 PM - 05:00 PM  │  M. Camba      │")
+    slowprint(" └──────────────┴───────────────────────────┴──────┴───────────────────────┴────────────────┘")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    opt = input(" Back to main (y/n): ")
+
+    if opt == 'y' or opt == 'Y':
+        main()
+    elif opt == 'n' or opt == 'N':
+        os.system("clear && exit")
+    else:
+        os.system("clear")
+        print(" Invalid input!")
+
+    
+def wednesday():
+    os.system("clear")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    os.system("toilet -f smbraille '           WEDNESDAY SCHEDULE'")
+    print("")
+    os.system("espeak 'today is your rest day so please get some rest'")
+    opt = input(" Back to main (y/n): ")
+
+    if opt == 'y' or opt == 'Y':
+        main()
+    elif opt == 'n' or opt == 'N':
+        os.system("clear && exit")
+    else:
+        os.system("clear")
+        print(" Invalid input!")
+    
+def thursday():
+    os.system("clear")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    os.system("toilet -f smbraille '          THURSDAY SCHEDULE'") 
+    print("")
+    slowprint(bcolors.BOLD + bcolors.GREEN + "\n ┌──────────────┬───────────────────────────┬──────┬───────────────────────┬────────────────┐")
+    slowprint(" │ Subject Code │  Descriptive Title        │ Day  │         Time          │  Instructor    │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITST 302     │  Client-Server Techs.     │ THU  │  07:70 AM - 10:30 AM  │  A. Cortez     │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITEP 310     │  Social and Pro. Issues   │ THU  │  10:30 AM - 11:30 AM  │  M. Camba      │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITEL 304     │  Integrative Programming  │ THU  │  01:00 PM - 03:00 PM  │  H. Natividad  │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITEP 309     │  Networking 2             │ THU  │  03:00 PM - 05:00 PM  │  H. Natividad  │")
+    slowprint(" └──────────────┴───────────────────────────┴──────┴───────────────────────┴────────────────┘")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    opt = input(" Back to main (y/n): ")
+
+    if opt == 'y' or opt == 'Y':
+        main()
+    elif opt == 'n' or opt == 'N':
+        os.system("clear && exit")
+    else:
+        os.system("clear")
+        print(" Invalid input!")
+
+
+def friday():
+    os.system("clear")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    os.system("toilet -f smbraille '            FRIDAY SCHEDULE'") 
+    print("")
+    slowprint(bcolors.BOLD + bcolors.GREEN + "\n ┌──────────────┬───────────────────────────┬──────┬───────────────────────┬────────────────┐")
+    slowprint(" │ Subject Code │  Descriptive Title        │ Day  │         Time          │  Instructor    │")
+    slowprint(" ├──────────────┼───────────────────────────┼──────┼───────────────────────┼────────────────┤")
+    slowprint(" │ ITST 301     │  Principle of Web Design  │ FRI  │  08:00 AM - 11:00 AM  │  M. Camba      │")
+    slowprint(" └──────────────┴───────────────────────────┴──────┴───────────────────────┴────────────────┘")
+    slowprint(bcolors.BOLD + bcolors.WARNING + "\n")
+    opt = input(" Back to main (y/n): ")
+
+    if opt == 'y' or opt == 'Y':
+        main()
+    elif opt == 'n' or opt == 'N':
+        os.system("clear && exit")
+    else:
+        os.system("clear")
+        print(" Invalid input!")
+
+
+if __name__=="__main__":
+    main()
+
